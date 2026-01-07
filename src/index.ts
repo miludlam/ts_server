466d35b7085ceadcd5c8ceda09176fc67b282fcb
Michael Ludlam <miludlam@gmail.com>
2026-01-06 21:43:33 -0500

    Set up metrics with a hit counter, reset handler, and API configs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponse } from "./api/middleware.js";
+import { handlerMetrics } from "./api/metrics.js";
+import { handlerReset } from "./api/reset.js";
+import {
+    middlewareLogResponse,
+    middlewareMetricsInc,
+} from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics);
+app.get("/api/reset", handlerReset);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
