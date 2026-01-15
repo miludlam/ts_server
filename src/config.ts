a44d98cb351e7c577d390a160ec11b3ec6a8e128
Michael Ludlam <miludlam@gmail.com>
2026-01-14 19:33:51 -0500

    Add a query for users and support for .env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
+process.loadEnvFile(".env");
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
+
+function envOrThrow(key: string): string {
+    const value = process.env[key];
+    if (!value) throw new Error(`Missing environment variable: ${key}`);
+    return value;
+}
 
 export const config: APIConfig = {
     fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
