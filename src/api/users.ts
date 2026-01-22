81353570b2daaa09c84549a712194e0454fd65d1
Michael Ludlam <miludlam@gmail.com>
2026-01-22 17:24:57 -0500

    Update user with a password field

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,20 +1,27 @@
 import type { Request, Response } from "express";
 
 import { createUser } from "../db/queries/users.js";
+import { hashPassword } from "./auth.js";
 import { ErrorBadRequest } from "./errors.js";
 import { responseJSON } from "./json.js";
 
 export async function handlerCreateUser(req: Request, res: Response) {
     type parameters = {
         email: string;
+        password: string;
     };
     const params: parameters = req.body;
 
     if (!params.email) {
         throw new ErrorBadRequest("Email is required");
     }
+    if (!params.password) {
+        throw new ErrorBadRequest("Password is required");
+    }
 
-    const user = await createUser({email: params.email});
+    const hashedPassword = await hashPassword(params.password);
+
+    const user = await createUser({email: params.email, hashed_password: hashedPassword});
     if (!user) {
         throw new Error("User could not be created");
     }
